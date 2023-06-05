ea2681b8a7df2989230bc65811f6822566ee4177
JlIlIllIlIIlIll <115548989+JlIlIllIlIIlIll@users.noreply.github.com>
2023-06-05 11:25:09 +0200

    About-me

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className="about__info grid">
+      <div className="about__box">
+        <h3 className="about__title">Student</h3>
+      </div>
+
+      <div className="about__box">
+        <h3 className="about__title"></h3>
+      </div>
+
+      <div className="about__box">
+        <h3 className="about__title"></h3>
+      </div>
+    </div>
+  )
+}
+
+export default Info
