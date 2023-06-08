534bffc82c6adb2dde41a4aafde7d47714627bf2
JlIlIllIlIIlIll <mohamed@ftouni.me>
2023-06-08 15:25:25 +0200

    Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import Nav from './components/nav/Nav'
-import Home from './components/home/Home'
-import About from './components/about/About'
-import Skills from './components/skills/Skills'
-import Qualification from './components/qualification/Qualification'
-import Contact from './components/contact/Contact'
+import React from "react";
+import Nav from "./components/nav/Nav";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
+import Qualification from "./components/qualification/Qualification";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
 
 const App = () => {
   return (
@@ -15,10 +16,11 @@ const App = () => {
         <About />
         <Skills />
         <Qualification />
+        <Portfolio />
         <Contact />
       </main>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import './portfolio.css'
+import React from "react";
+import "./portfolio.css";
+import Project1 from "../../assets/ASTROCAT.png";
 
 const Portfolio = () => {
+  const data = [
+    {
+      id: 1,
+      name: "Project 1",
+      img: Project1,
+    },
+    {
+      id: 2,
+      name: "Project 2",
+      img: Project1,
+    },
+    {
+      id: 3,
+      name: "Project 3",
+      img: Project1,
+    },
+    {
+      id: 4,
+      name: "Project 4",
+      img: Project1,
+    },
+  ];
   return (
-    <div>Portfolio</div>
-  )
-}
+    <section className="portfolio section" id="portfolio">
+      <h2 className="section__title">Portfolio</h2>
+      <span className="section__subtitle">My work</span>
+      <div className="portfolio__container container grid">
+        <div className="portfolio__content">
+          {data.map((item) => (
+            <div key={item.id} className="portfolio__item">
+              <img src={item.img} alt={item.name} className="portfolio__img" />
+              <h3 className="portfolio__name">{item.name}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
